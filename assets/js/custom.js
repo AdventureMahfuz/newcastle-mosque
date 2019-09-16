7573edda51f55a1c8c81df712bddc97ebccfb3f8
AdventureMahfuz <adventure.mahfuz@gmail.com>
2019-09-16 20:30:46 +0600

    designed front page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,22 @@
 (function($) {
 $(document).ready(function(){
+    //back to top show when scroll
+    $(".back_to_top i.fa").css("display", "none");
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 250) {
+            $(".back_to_top i.fa").css("display", "none");
+        } else {
+            $(".back_to_top i.fa").css("display", "block");
+        }
+    });
+    $(".back_to_top i.fa").on("click", function () {
+        $("html").animate({
+            scrollTop: 0
+        }, 800);
+    });
+
+
     //hero slider
     $('.hero-slider').owlCarousel({
         animateIn: 'fadeIn',
@@ -38,6 +55,32 @@ $(document).ready(function(){
 
 
 
+    //team-members slider
+    $('.team-members').owlCarousel({
+        loop:true,
+        margin:30,
+        autoplay: true,
+        responsiveClass:true,
+        nav:true,
+        dots:false,
+        mouseDrag:true,
+        touchDrag: true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:4,
+                nav:true
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:true
+            }
+        }
+    });
 
     //WoW js activation
     //new WOW().init();
